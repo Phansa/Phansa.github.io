99e8286c38eceb1d928bd38f5793b0a262695afa
Adeet <blaze5545@gmail.com>
2016-12-30 14:21:09 -0500

    adding new code

--- javascripts/fitch.js
+++ javascripts/fitch.js
@@ -1,13 +1,23 @@
 function fitch_one()
 {	
-	solution(fitch("P","operation", "Q", "operation_one"));
+	var one = fitch("P","operation", "Q", "operation_one");
+	if(one)
+	{
+		solution(compare_to(one, "operation"));
+	}
+	else
+	{
+		solution(false);
+	}
+	//solution(compare_to(fitch("P","operation", "Q", "operation_one"), "operation"));
 }
 function fitch(a,b,c,d)
 {
+	//alert(document.getElementById("operation_one").value);
 	var P = document.getElementById(a).value;
 	if(!valid_input(P))
 	{
-		alert("Please enter valid input into" + a);
+		alert("Please enter valid input into " + a);
 		return false;
 	}
 	var OP = document.getElementById(b).value;
@@ -19,34 +29,75 @@ function fitch(a,b,c,d)
 	var Q = document.getElementById(c).value;
 	if(!valid_input(Q))
 	{
-		alert("Please enter valid input into" + c);
+		alert("Please enter valid input into " + c);
 		return false;
 	}
     P = convert_to_bool(P);
 	Q = convert_to_bool(Q);
 	OP = convert_to_bool(OP);
 	var e = document.getElementById(d).value;
-	if(e == 1)
+	var result = evaluate(P,Q,e);
+	return result;
+}
+function fitch2(bool1, op2, field2, operation_combo)
+{
+	var OP = document.getElementById(op2).value;
+	if(!valid_input(OP))
+	{
+		alert("Please enter valid input into " + op2);
+		return false;
+	}
+	var Q = document.getElementById(field2).value;
+	if(!valid_input(Q))
+	{
+		alert("Please enter valid input into " + field2);
+		return false;
+	}
+	var e = document.getElementById(operation_combo).value;
+	return evaluate(bool1, Q, e);
+}
+function evaluate(P,Q,op)
+{
+	if(op == 1)
 	{
 		var result = (P || !Q);
 	}
-	if(e == 2)
+	if(op == 2)
 	{
 		var result = (P && Q);
 	}
-	if(e == 3)
+	if(op == 3)
 	{
 		var result = (P || Q);
 	}
-	if(e == 4)
+	if(op == 4)
 	{
 		var result = ((P || !Q) && (!P || Q));
 	}
-	return (result && OP);
+	return result;
+}
+function compare_to(result, field)
+{
+	return(result && convert_to_bool(document.getElementById(field).value));
 }
 function fitch_two()
 {
-	solution(fitch("P1","OP11","Q1", "operation_two_one") && fitch("Q1", "OP12", "R1", "operation_two_two"))
+	var one = fitch("P1","OP11","Q1", "operation_two_one");
+	if(one)
+	{
+		if(compare_to(one, "OP11"))
+		{
+			var two = fitch2(one, "OP12", "R1", "operation_two_two")
+			if(two)
+			{
+				solution(compare_to(two, "OP12"));
+			}
+		}
+		else
+		{
+			solution(false);
+		}
+	}
 }
 function valid_input(a)
 {
