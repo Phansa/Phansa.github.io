767f4126dadca0eee91750d655767a987396beea
Adeet <blaze5545@gmail.com>
2017-01-03 11:24:01 -0500

    adding new images

--- javascripts/music.js
+++ javascripts/music.js
@@ -3,12 +3,12 @@ var sources =[];
 sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/229577943&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
 sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/232534809&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
 sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/229175825&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
-sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/197702891&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false");
+sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/197702891&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false");
 var song_titles = [];
-song_titles.push("You are listening to Le Coeur - Cutiecore");
-song_titles.push("You are listening to Wasted - Lofts");
-song_titles.push("You are listening to All I Ask - Essence");
-song_titles.push("You are listening to Lost - Fan Fiction");
+song_titles.push("(◕‿◕✿) You are listening to Le Coeur - Cutiecore (◕‿◕✿)");
+song_titles.push("(づ ◕‿◕ )づ You are listening to Wasted - Lofts (づ ◕‿◕ )づ");
+song_titles.push("(´• ω •`)ﾉ	 You are listening to All I Ask - Essence (´• ω •`)ﾉ	");
+song_titles.push(".･ﾟﾟ･(／ω＼)･ﾟﾟ･. You are listening to Lost - Fan Fiction .･ﾟﾟ･(／ω＼)･ﾟﾟ･.");
 function next()
 {
 	if(x < sources.length - 1)
@@ -21,6 +21,7 @@ function next()
 	}
 	document.getElementById("CurrentSong").src = sources[x];
 	document.getElementById("Song Title").innerText = song_titles[x];
+	change();
 }
 function prev()
 {
@@ -34,4 +35,24 @@ function prev()
 	}
 	document.getElementById("CurrentSong").src = sources[x];
 	document.getElementById("Song Title").innerText = song_titles[x];
+	change();
+}
+function random()
+{
+	x = Math.floor(Math.random() * (3 - 0 + 1));
+	document.getElementById("CurrentSong").src = sources[x];
+	document.getElementById("Song Title").innerText = song_titles[x];
+	change();
+}
+function change()
+{
+	if(x == 3)
+	{
+		document.getElementById("MainImage").src = "http://dazedimg.dazedgroup.netdna-cdn.com/640/azure/dazed-prod/1160/4/1164711.jpg";
+		document.getElementById("MainImage").width = 400;
+	}
+	else
+	{
+		document.getElementById("MainImage").src = "images/Music/djpon3.gif";
+	}
 }
