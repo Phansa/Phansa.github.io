a372690f3390e04f1bcf02c09faf6a455c6a72ed
Phansa <blaze5545@gmail.com>
2017-01-25 21:36:26 -0500

    adding new image

--- javascripts/music.js
+++ javascripts/music.js
@@ -12,16 +12,20 @@ sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/
 sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/230222227&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
 sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/210503956&amp;color=ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false");
 sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/234739832&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/233798320&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/217970408&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
 var song_titles = [];
 song_titles.push("(◕‿◕✿) Le Cur - Cutiecore (◕‿◕✿)");
 song_titles.push("(づ ◕‿◕ )づ Wasted - Lofts (づ ◕‿◕ )づ");
-song_titles.push("(´• ω •`)ﾉ	 All I Ask - Essence (´• ω •`)ﾉ	");
-song_titles.push(".･ﾟﾟ･(／ω＼)･ﾟﾟ･. Lost - Fan Fiction .･ﾟﾟ･(／ω＼)･ﾟﾟ･.");
+song_titles.push("(´• ω •`)ﾉ	 All I Ask - Skrillex Ft. Penny (´• ω •`)ﾉ	");
+song_titles.push(".･ﾟﾟ･(／ω＼)･ﾟﾟ･. Lost - Scavenger Hunt (Lenno Remix) .･ﾟﾟ･(／ω＼)･ﾟﾟ･.");
 song_titles.push("(=①ω①=) Cat Cafe - Yoshi & Komono (=①ω①=)");
-song_titles.push("♡＼(￣▽￣)／♡ Begin Again - Fan Fiction ♡＼(￣▽￣)／♡");
-song_titles.push("╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ	 Cool Teens - Ignite ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ");
-song_titles.push("o( ❛ᴗ❛ )o Stars - Cool Teens o( ❛ᴗ❛ )o");
-song_titles.push("٩(｡•́‿•̀｡)۶	Down - Babe Island ٩(｡•́‿•̀｡)۶	");
+song_titles.push("♡＼(￣▽￣)／♡ Begin Again - Purity Ring ♡＼(￣▽￣)／♡");
+song_titles.push("╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ	 Waiting to Ignite - Goldroom ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ");
+song_titles.push("o( ❛ᴗ❛ )o Gregory Esayan - Monday Night (feat. Ai Takekawa) o( ❛ᴗ❛ )o");
+song_titles.push("٩(｡•́‿•̀｡)۶	We All Fall Down - A-Track ft. Jamie Lidell ٩(｡•́‿•̀｡)۶	");
+song_titles.push("(✿◕﹏◡)~★ Been You - Justin Bieber (✿◕﹏◡)~★");
+song_titles.push("(^=◕ᴥ◕=^) Everything Is Embarassing - Sky Ferreira (^=◕ᴥ◕=^)");
 function next()
 {
 	if(x < sources.length - 1)
@@ -62,15 +66,40 @@ function change()
 {
 	if(x == 0)
 	{
-		document.getElementById("MainImage").src = "images/Music/djpon3.gif";	
+		temp = Math.floor(Math.random() * 2);
+		if(temp == 1)
+		{
+			document.getElementById("MainImage").src = "images/Music/djpon3.gif";	
+		}
+		else
+		{
+			//Source - https://thepowerzone.files.wordpress.com/2013/01/before-watchmen-minutemen-1.jpg
+			document.getElementById("MainImage").src = "images/Music/Watchmen_Start.jpg";
+		}
 	}
 	else if(x == 1)
 	{
-		document.getElementById("MainImage").src = "images/Music/fionna.jpg";
+		temp = Math.floor(Math.random() * 2);
+		if(temp == 1)
+		{
+			document.getElementById("MainImage").src = "images/Music/fionna.jpg";
+		}
+		else
+		{
+			document.getElementById("MainImage").src = "images/Music/BioshockCute.jpg";
+		}
 	}
 	else if(x == 2)
 	{
-		document.getElementById("MainImage").src = "images/Music/penny.jpg";
+		temp = Math.floor(Math.random() * 2);
+		if(temp == 1)
+		{
+			document.getElementById("MainImage").src = "images/Music/penny.jpg";
+		}
+		else
+		{
+			document.getElementById("MainImage").src = "http://40.media.tumblr.com/95998eb7964ebe7a0c1c14bd0813ebf4/tumblr_nw7kzuIY4B1sk51m7o1_r1_500.jpg";
+		}
 	}
 	else if(x == 3)
 	{
@@ -79,11 +108,20 @@ function change()
 	}
 	else if(x == 4)
 	{
-		document.getElementById("MainImage").src ="https://s-media-cache-ak0.pinimg.com/originals/d4/e2/85/d4e285ca9d7d65116c42caffca2a7fb9.png";
+		temp = Math.floor(Math.random() * 2);
+		if(temp == 1)
+		{
+			document.getElementById("MainImage").src ="https://s-media-cache-ak0.pinimg.com/originals/d4/e2/85/d4e285ca9d7d65116c42caffca2a7fb9.png";
+		}
+		else
+		{
+			//Source - http://img13.deviantart.net/0c93/i/2016/310/2/5/genesis___pharah_x_mercy_by_istarlove-daaacax.jpg
+			document.getElementById("MainImage").src = "images/Music/PharahMercy.jpg";
+		}
 	}
 	else if(x == 5)
 	{
-		var temp = Math.floor(Math.random() * 2);
+		temp = Math.floor(Math.random() * 2);
 		if(temp == 1)
 		{
 			//Source http://static2.comicvine.com/uploads/original/13/132327/2840382-ozym_cv6_var.jpg
@@ -97,18 +135,18 @@ function change()
 	}
 	else if(x == 6)
 	{
-		var temp = Math.floor(Math.random() * 3);
-		if(temp == 0)
+		temp = Math.floor(Math.random() * 3);
+		if(temp == 1)
 		{
 			//Source http://static.zerochan.net/Litten.full.1999835.jpg
 			document.getElementById("MainImage").src = "images/Music/litten.jpg";
 		}
-		else if(temp == 1)
+		else if(temp == 2)
 		{
 			//Source http://pre09.deviantart.net/06a1/th/pre/f/2016/131/e/6/litten_by_hikariviny-da253cu.png
 			document.getElementById("MainImage").src = "images/Music/litten2.png";
 		}
-		else if(temp == 2)
+		else
 		{
 			//Source http://pre11.deviantart.net/9545/th/pre/i/2016/188/0/a/litten_for_president__by_arkadark-da95400.jpg
 			document.getElementById("MainImage").src = "images/Music/litten3.jpg";
@@ -116,7 +154,7 @@ function change()
 	}
 	else if(x == 7)
 	{
-		var temp = Math.floor(Math.random() * 2);
+		temp = Math.floor(Math.random() * 2);
 		if(temp == 1)
 		{
 			document.getElementById("MainImage").src = "images/Music/DVA_Arcade.png";
@@ -128,7 +166,7 @@ function change()
 	}
 	else if(x == 8)
 	{
-		var temp = Math.floor(Math.random() * 4);
+		temp = Math.floor(Math.random() * 4);
 		if(temp == 1)
 		{
 			document.getElementById("MainImage").src = "https://images-na.ssl-images-amazon.com/images/I/51nV51JA-pL._SS500.jpg";
@@ -146,6 +184,31 @@ function change()
 		else
 		{
 			document.getElementById("MainImage").src = "images/Music/shaco_main.jpg";
+		}
+	}
+	else if(x == 9)
+	{
+		temp = Math.floor(Math.random() * 2);
+		if(temp == 1)
+		{
+			document.getElementById("MainImage").src = "http://media.comicbook.com/2016/05/watchmen-dr--manhattan-hd-wallpapers-184238.jpg";
+		}
+		else
+		{
+			//Image source - https://www.nineoverten.com/wp-content/uploads/2012/09/SilkSpectre3.jpg
+			document.getElementById("MainImage").src = "images/Music/Comedian.jpg";
+		}
+	}
+	else if(x == 10)
+	{
+		temp = Math.floor(Math.random() * 2);
+		if(temp == 1)
+		{
+			document.getElementById("MainImage").src = "http://68.media.tumblr.com/2e1627dfec4c81264bf6c56b0ff8c646/tumblr_n52m5uyQsp1qba3qpo1_500.png";
+		}
+		else
+		{
+			document.getElementById("MainImage").src = "https://s-media-cache-ak0.pinimg.com/736x/0c/70/bf/0c70bffe17bd387cd35d359769da0bd5.jpg";
 		}
 	}
 }
