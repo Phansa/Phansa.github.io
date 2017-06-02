a6c8bd03c6a5c37484506e6868485cad42218f59
Phansa <blaze5545@gmail.com>
2017-06-02 00:13:18 -0400

    updating song title

--- javascripts/music.js
+++ javascripts/music.js
@@ -35,7 +35,7 @@ sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/
 //TO ADD - A.G. Cook Beuatiful. Find right images for it~
 //sources.push("https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/152774056&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
 var song_titles = [];
-song_titles.push("(◕‿◕✿) Le Cur - Cutiecore (◕‿◕✿)");
+song_titles.push("(◕‿◕✿) Acide - Jenn Ayache  (◕‿◕✿)");
 song_titles.push("(づ ◕‿◕ )づ Wasted - Lofts (づ ◕‿◕ )づ");
 song_titles.push("(´• ω •`)ﾉ	 All I Ask - Skrillex Ft. Penny (´• ω •`)ﾉ	");
 song_titles.push(".･ﾟﾟ･(／ω＼)･ﾟﾟ･. Lost - Scavenger Hunt (Lenno Remix) .･ﾟﾟ･(／ω＼)･ﾟﾟ･.");
