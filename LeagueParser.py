ef3ed27a68b565af4376c6753a10f7eee8cdf24d
Phansa <blaze5545@gmail.com>
2017-07-12 20:18:35 -0400

    moving all images locally

--- LeagueParser.py
+++ LeagueParser.py
@@ -3,6 +3,8 @@ import pandas
 import math
 import numpy as np
 import xlsxwriter
+import urllib.request as ur
+import os
 
 
 #The base of this program was taken from this tutorial on how to access the Riot Games API which I found very helpful!
@@ -51,8 +53,7 @@ def main():
         #********************Start Program************************
         #Used in my getChampionData function. API Key has been commented out for security reasons.
         region = "na"
-        #Insert your own API Key below
-        APIKey = "INSERT KEY HERE"
+        APIKey = "APIKEY"
         #Will store the skins I own read in from an excel worksheet
         skins_list = []
         champion_names = []
@@ -61,7 +62,6 @@ def main():
         skins_list_temp = [[]]
         #The base URL for every skin is this. This program will be searching for the rest of the
         #url to complete and making a HTML Document containing all the url links to the skins I currently own.
-        #skin_url = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"
         skin_url = "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/"
         output_url_list = [[]]
         #Reading in the excel file, removing all NaN/empty cell values and replacing them with empty string
@@ -72,7 +72,7 @@ def main():
         champ_name = ""
         count = 0
         for index, row in df.iterrows():
-            #Intialzie my temporary skin list
+            #Initialize my temporary skin list
             skins_list_temp_initialize = []
             skins_list_temp.append(skins_list_temp_initialize)
             for x in range (1,10):
@@ -84,13 +84,16 @@ def main():
             champion_names_temp.append(row[0])
             count += 1;
         test = getChampionData(region, APIKey)
+        if(not(os.path.isdir("images/LeagueOfLegends"))):
+                os.makedirs("images/LeagueOfLegends")
+
         #For the Splash URLs we can't use the direct champion names but instead the champion names riot uses
         #which is what this function finds. For example Aurelion Sol is known as AurelionSol
         for i in test['data']:
             champion_names.append(i)
         #This list will be used to remove skins as they are found to reduce the run time 
         list_to_be_removed = []
-        #Loops through each champion form the JSON object
+        #Loops through each champion from the JSON object
         index_to_insert = 0;
         bool_found = False
         for x in range (0, len(champion_names)):
@@ -106,7 +109,6 @@ def main():
             else:
                 for y in range(0, len(output_url_list)):
                     if(output_url_list[y][0] > champion_names[x]):
-                        #print(output_url_list[y][0], champion_names[x])
                         index_to_insert = y
                         bool_found = True
                         break
@@ -117,40 +119,42 @@ def main():
                     output_url_list.insert(index_to_insert, output_url_initialize_list)
             #Loops through all for a specific champion and skins from my input list
             master_skin_list = test['data'][champion_names[x]]['skins']
+            #Creates a directory to store this champions images
+            if(not(os.path.isdir("images/LeagueOfLegends/" + champion_names[x]))):
+                os.makedirs("images/LeagueOfLegends/" + champion_names[x])
+            #output_url_list[index_to_insert].pop(0)
+            output_url_list[index_to_insert].append("<img src=" + "images/LeagueOfLegends/" + champion_names[x] + "/default.jpg>")
+            #output_url_list.pop(0)
             for y in range(0, len(master_skin_list)):
                 for z in range(0, len(skins_list)):
-                    #Checks to see if this champion has any skins that I own, and if yes appends the url to
-                    #my output_url list as well as puts the skin in the to be removed list.
+                    #Creates a local copy of each skin image
+                    source_image_link = skin_url + champion_names[x] + '_' + str(master_skin_list[y]['num']) + ".jpg"
+                    image_path = "images/LeagueOfLegends/" + champion_names[x] + "/" + "".join(master_skin_list[y]['name'].split()) + ".jpg"
+                    if(not(os.path.isfile(image_path))):
+                        opened_image_link = ur.urlopen(source_image_link)
+                        out_path = open(image_path,
+                                                "wb")
+                        out_path.write(opened_image_link.read())
+                        out_path.close()
+                    #Checks to see if this champion has any skins that I own, and if yes appends the relative path
+                    #my output_url list
                     if(skins_list[z] == master_skin_list[y]['name']):
-                        output_url_list[index_to_insert].append("<img src=" + skin_url + champion_names[x] + '_' + str(master_skin_list[y]['num']) + ".jpg" + ">")
+                        output_url_list[index_to_insert].append("<img src=" + image_path + ">")
+                        #print(source_image_link)
+                        #output_url_list[index_to_insert].append("<img src=" + skin_url + champion_names[x] + '_' + str(master_skin_list[y]['num']) + ".jpg" + ">")
             bool_found = False
-            #if(x < 3):
-                #print(list)
-                #print(output_url_list)
-                        #list_to_be_removed.append(skins_list[z])
-                #for z in range (0, len(list_to_be_removed) - 1):
-                    #skins_list.remove(list_to_be_removed[z])
-                #del list_to_be_removed[:]
-        #Used for Debugging
-        #print(test['data']['Anivia']['skins'])
-        #list = test['data']['Anivia']['skins']
-        #for x in range(0, len(list) - 1):
-            #print(list[x])
-        #print(output_url_list)
-        #print(champion_names_temp)
-        #print(skins_list_temp)
+ 
         
         #My first idea was to write the Skin + Champion name to a textfile then manually edit the excel document with the
         #updated names, however I realized I could use a similar method as what I was doing and just write directly to a new
         #excel worksheet.
-        out = open("names.txt", "w+")
-        for x in range(0, len(champion_names_temp)):
-            for z in range(0, len(skins_list_temp[x])):
-                out.write(skins_list_temp[x][z] + " " + champion_names_temp[x])
-                skins_list_temp[x][z] = skins_list_temp[x][z] + " " + champion_names_temp[x]
-                out.write("\n")
-        out.write("hello")
-        #print(skins_list_temp)
+        #out = open("names.txt", "w+")
+        #for x in range(0, len(champion_names_temp)):
+        #    for z in range(0, len(skins_list_temp[x])):
+        #        out.write(skins_list_temp[x][z] + " " + champion_names_temp[x])
+        #        skins_list_temp[x][z] = skins_list_temp[x][z] + " " + champion_names_temp[x]
+        #        out.write("\n")
+        #
         #I was going to use a dataframe/pandas method but I decided to use xlsxwriter as I had used a similar method 
         #when making my database applicaiton.
         #d = {'Champion': champion_names_temp}
@@ -218,11 +222,9 @@ def main():
 \n \n""")
         for x in range(0, len(output_url_list)):
             for z in range(0, len(output_url_list[x])):
-                if(z == 0):
-                    out2.write("<img src=" + skin_url + output_url_list[x][z] + "_0.jpg" + ">")
-                else:
+                if(z != 0):
                     out2.write(output_url_list[x][z])
-                out2.write("\n")
+                    out2.write("\n")
             out2.write("<br />\n")
         out2.write("</div>\n</div>\n")
         out2.write("</html>")
