82556caa1484f8f159c14a1bf985c6bba65b40af
Phansa <blaze5545@gmail.com>
2018-07-09 19:58:13 -0400

    adding start of react website

--- src/App.js
+++ src/App.js
@@ -1,68 +1,38 @@
-import './App.css';
 import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
-
-const Home = () => (
+import Home from './Home.js';
+import './core.css';
+const LeagueSkins = () => (
   <div>
-    <h2>Home</h2>
+    <h1>
+      Work in Progress
+    </h1>
   </div>
 )
 
-const About = () => (
+const Photos = ({ match }) => (
   <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic}/>
-    <Route exact path={match.path} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
+    <h1>
+      Work in Progress
+    </h1>
   </div>
 )
 
 const NavBar = () =>
 (
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
+  <nav className="navbar navbar-inverse navbar-fixed-top">
+  <div className="container">
+    <div className="navbar-header">
     </div>
-    <div id="navbar" class="collapse navbar-collapse">
-      <ul class="nav navbar-nav">
+    <div id="navbar" className="collapse navbar-collapse">
+      <ul className="nav navbar-nav">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
+        <li><Link to="/leagueSkins">League Skins</Link></li>
+        <li><Link to="/photos">Photos</Link></li>
       </ul>
     </div>
   </div>
@@ -77,8 +47,8 @@ const App = () => (
       <hr/>
 
       <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
+      <Route path="/leagueSkins" component={LeagueSkins}/>
+      <Route path="/photos" component={Photos}/>
     </div>
   </Router>
 )
---
+++ src/Home.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+const Home = () =>
+(
+    <section>
+        <h1>
+            About Me.
+        </h1>
+        <div id="SelfImage">
+            <img src="./images/Photos/SelfPortrait.jpg" alt="Me" />
+        </div>  
+        <p>
+            My name is Adeet Phanse and I am currently a Software Engineer at 
+            Capital One. I have a degree in Computer Science
+            from Rensselaer Polytechnic Institute and am interested in 
+            web/full stack development. On this website you can find some of my 
+            hobbies/interests as well as projects I have worked on.
+            <br /> 
+            Feel free to add me on LinkedIn 
+            <a href="https://www.linkedin.com/in/adeet-phanse-4a07b1100">
+            <img src="./images/Photos/linkedin_image.png" width ="40" height="40" border="0" /> 
+            </a> 
+        </p>
+        <h1>
+            Hobbies/Interests
+        </h1>
+        <p> 
+            I sell various Magic the Gathering / Yu-Gi-Oh cards that I have 
+            collected over the years on these following sites, Tcgplayer.com and eBay.com. 
+        </p>
+        <p> Here are links to my stores!
+            <br />
+            Ebay - 
+            <a href="http://www.ebay.com/sch/eletrix214/m.html?_nkw=&_armrs=1&_ipg=&_from=">
+            <img src="./images/Photos/ebay_image.png" width="646" height="220" border="0" />
+            </a>
+            <br />
+            Tcgplayer - 
+            <a href="http://shop.tcgplayer.com/all/product/show?sellerfilter=ff9a806a
+            &newSearch=false&Language=English&lu=true">
+            <img src="./images/Photos/tcgplayer.png" border="0" />
+            </a>
+        </p>
+        <br />
+        <p> Check out my Github!
+            <a href="https://github.com/Phansa"> 
+            <img src="https://image.flaticon.com/icons/svg/25/25231.svg" 
+            height="150" width="100"border ="0" /> 
+            </a>
+        </p>
+        <h1>
+            Computer Science Projects
+        </h1>
+        <p>
+            I have created a database application in Visual Basic to keep track of 
+            membership information for India Foundation of Metropolitan Princeton. 
+            More details about both of those projects can be found <a href="projects.html">
+            here </a>.
+        </p>
+        <p>
+            I have worked on another open source Pokemon game simulator, 
+            <a href="https://github.com/FullScreenShenanigans/FullScreenPokemon"> 
+            FullScreen Pokemon </a> which is a TypeScript implementation of the original Pokemon 
+            games with an emphasis on extensibility  and modularity. I worked on various parts of 
+            the game engine, fixed some bugs and even animated some moves! It is a very cool and 
+            ambitious project and I would encourage you to check it out or even contribute to it!
+        </p>
+        <p>
+            I am always working on various websites and hosting them on github, here a few of 
+            my most recent ones.
+            <br />
+            <a href="https://phansa.github.io/Snake/"> Snake </a> - a simple javascript "snake" like game.
+            <br />
+            <a href="https://phansa.github.io/NostalgiaMTG-Angular/"> Nostalgia MTG </a> - 
+            a Magic the Gathering 
+            pack opening simulator.
+            <br />
+            <a href="https://phansa.github.io/ReactLearning/"> Mining Simulator </a> - 
+            a cookie clicker - esque mining game.
+            <br />
+            <a href="https://phansa.github.io/OldSchoolRunescapeClueGenerator/"> Clue Generator </a> -
+            a clue scroll reward generator based on the game Old School Runescape.
+        </p>
+        <h1> 
+            Thanks for visiting my website and have a great day!
+        </h1>
+    </section>
+)
+export default Home;
